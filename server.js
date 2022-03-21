f914ec10b922cc31c8694191d97c98ce111e3603
michaelrios2015 <michael.rios093@gmail.com>
2022-03-20 21:23:35 -0400

    start of database

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
- console.log('hello world') 
+// this does the magic of leeting us use postgres
+const pg = require('pg');
+
+// here the pg>clien lets us connect to a database
+const client = new pg.Client('postgres://postgres:JerryPine@localhost/depts_classes');
+
+
+
+
+// we try to actually connect it could fail so the try an dcatch
+// it's not fast so async
+const setUP = async() => {
+    try {
+      await client.connect();
+      console.log('connected to database');
+    }
+    catch(ex){
+        console.log(ex)
+    }
+
+}
+
+setUP();
