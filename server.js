65861f8eb379b844aa494c9b37c851e606b6c50b
michaelrios2015 <michael.rios093@gmail.com>
2022-03-20 21:31:34 -0400

    more database

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ const pg = require('pg');
 // here the pg>clien lets us connect to a database
 const client = new pg.Client('postgres://postgres:JerryPine@localhost/depts_classes');
 
+// we used this often... I started liking to just create everything outside of the app
+// and just connect to the app from the database 
+const syncAndSeed = async() => {
+    const SQL = `
+    CREATE TABLE IF NOT EXISTS depts(
+        id INTEGER PRIMARY KEY,
+        name VARCHAR(100) NOT NULL
+    );
+    `;
 
+    await client.query(SQL);
+};
 
 
 // we try to actually connect it could fail so the try an dcatch
@@ -12,6 +23,7 @@ const client = new pg.Client('postgres://postgres:JerryPine@localhost/depts_clas
 const setUP = async() => {
     try {
       await client.connect();
+      await syncAndSeed();
       console.log('connected to database');
     }
     catch(ex){
